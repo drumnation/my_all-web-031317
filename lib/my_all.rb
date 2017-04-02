395b82091d01b85232ed066dad376d45b03316f6
drumnation <davidmieloch@gmail.com>
2017-04-02 14:22:05 -0400

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,24 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  block_return_values = []
+  # 3. Save the return value of the yield block
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+    # 1. iterate through each element in the passed in collection
+    # 2. yield each element in the collection to the block
+  end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
+    # 4. Determine the return value of the method
+  end
 
 end
+
+# my_all?([1,2,3]) {|i| i < 2}
+# => block_return_values = [true, false, false]
